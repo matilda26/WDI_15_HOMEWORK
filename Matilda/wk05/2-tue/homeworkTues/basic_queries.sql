4377b6df51da4b2da88b13cc4a6bb725dbb42e84
Matilda Rutherford <rutherford.matilda@gmail.com>
2018-04-11 11:13:34 +1000

    database homework

---
+++ Matilda/wk05/2-tue/homeworkTues/basic_queries.sql
@@ -0,0 +1,33 @@
+-- BASIC QUERIES
+-- Unless otherwise stated, all queries should return all columns
+
+-- Get all information about all authors
+SELECT * FROM authors;
+-- Get just the name and birth year of all authors
+SELECT name, birth_year FROM authors;
+-- Get all authors born in the 20th centruy or later
+SELECT * FROM authors WHERE birth_year > 1899;
+-- Get all authors born in the USA
+SELECT * FROM authors WHERE nationality = 'United States of America';
+-- Get all books published on 1985
+SELECT * FROM books WHERE publication_date = 1985;
+-- Get all books published before 1989
+SELECT * FROM books WHERE publication_date < 1989;
+-- Get just the title of all books.
+SELECT title FROM books;
+-- Get just the year that 'A Dance with Dragons' was published
+  -- Cry when you realize how long it's been
+SELECT publication_date FROM books WHERE title = 'A Dance with Dragons';
+-- Get all books which have `the` somewhere in their title (hint, look up LIKE/ILIKE)
+SELECT * FROM books WHERE title ILIKE '%the%';
+-- Add yourself as an author
+INSERT INTO authors(name, nationality, birth_year) VALUES ('Matilda Rutherford', 'Australia', 1993);
+-- Add two books that you'd like to write (you can hard-code your id as the author id)
+INSERT INTO books(title, publication_date, author_id) VALUES ('Small Town Girl', 2004, 10);
+INSERT INTO books(title, publication_date, author_id) VALUES ('Party Time', 2011, 10);
+-- Update one of your books to have a new title
+UPDATE books SET title = 'Small Town Tomboy' WHERE title = 'Small Town Girl';
+-- Delete your books
+DELETE FROM books WHERE author_id = 10;
+-- Delete your author entry
+DELETE FROM authors WHERE id = 10;
