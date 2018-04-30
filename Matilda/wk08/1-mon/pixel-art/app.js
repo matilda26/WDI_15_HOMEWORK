ff9dfe2113fdd975c5412bf97dd43aa0c0d3840b
Matilda Rutherford <rutherford.matilda@gmail.com>
2018-04-30 16:06:58 +1000

    stage two commit - use enter to trigger input submit

--- Matilda/wk08/1-mon/pixel-art/app.js
+++ Matilda/wk08/1-mon/pixel-art/app.js
@@ -1,8 +1,15 @@
 var colorSample = document.querySelector('.color-sample');
 var colorSubmit = document.querySelector('.color-btn');
+var colorInput = document.querySelector('.color-input');
 
 
 colorSubmit.addEventListener('click', function(event) {
-  colorSample.style.backgroundColor = document.querySelector('.color-input').value;
-
+  colorSample.style.backgroundColor = colorInput.value;
 })
+
+colorInput.addEventListener("keyup", function(event) {
+  event.preventDefault();
+  if (event.keyCode === 13) {
+    colorSubmit.click();
+  }
+});
